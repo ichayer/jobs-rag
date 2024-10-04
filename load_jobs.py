34398dbe6f6accc4adf850b66b13779e4eaf7fdf
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2024-10-03 22:01:38 -0300

    Add missing env variable, specify CV file via arg

--- load_jobs.py
+++ load_jobs.py
@@ -4,7 +4,7 @@ from utils.utils import load_env_vars, check_python_version, suppress_warnings
 
 if __name__ == "__main__":
     check_python_version()
-    env_vars = load_env_vars(["INDEX_NAME", "PINECONE_API_KEY", "JOBS_FOLDER"])
+    env_vars = load_env_vars(["INDEX_NAME", "PINECONE_API_KEY", "JOBS_FOLDER", "COMPUTE_DEVICE"])
     suppress_warnings()
 
     document_loader = DocumentLoader(jobs_folder_path=env_vars["JOBS_FOLDER"])
--- recommend_jobs.py
+++ recommend_jobs.py
@@ -1,3 +1,4 @@
+import sys
 from models.rag import RAG
 from utils.utils import check_python_version, suppress_warnings
 
@@ -5,7 +6,11 @@ if __name__ == "__main__":
     check_python_version()
     suppress_warnings()
 
-    best_match, sorted_jobs = RAG().run_with_scores(pdf_path="cvs/junior-full-stack-developer-resume-example.pdf", k=5)
+    if len(sys.argv) != 2:
+        print("You must specify one parameter with the name of the CV file")
+        exit(1)
+
+    best_match, sorted_jobs = RAG().run_with_scores(pdf_path=sys.argv[1], k=5)
     print(f"Recommended job source: {best_match['source']} with a final score of {best_match['final_score']:.2f}")
     for result in sorted_jobs:
         print(f"Source: {result['source']}, Final Score: {result['final_score']:.2f}")
